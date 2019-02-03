c4ed78696e76ea90fa9e4691175f5eeb99aab9f1
Gen Frenot <46330597+GenFrenot@users.noreply.github.com>
2019-02-03 09:28:00 +0100

    final version re-tested

--- run_analysis.R
+++ run_analysis.R
@@ -46,55 +46,62 @@ subject     <- read.table("test/subject_test.txt") %>% rbind(read.table("train/s
 y           <- read.table("test/y_test.txt")       %>% rbind(read.table("train/y_train.txt"))
 X           <- read.table("test/X_test.txt")       %>% rbind(read.table("train/X_train.txt"))
 
+# Reading the list of the 561 measurements gi give nice names to X columns
+features <- read.table("features.txt")
+
+# Renaming the X variable names with the features names
+colnames(X)<-features[,2]
+
+# The data frame "subject" has 10299 observations of 1 variable: the subject who performed the activity for each window sample.
+# The data frame "y_labels" has 10299 observations of 2 variable: head(
+#                                      V1 is the id and V2 is the description of the activity performed for each window sample.
+# The data frame "selected_X" has 10299 observation of 79 mean and std variables recorded by the phone sensor
+#                                      tBodyAcc-mean()-Y..fBodyBodyGyroJerkMag-meanFreq()
+#
+# Binding the subject, activity and features together as one data frame
+data<-cbind(subject=subject$V1,activity_id=y$V1,X)
+
+#intermediary verification
+#write.table(data,file="data4.txt", row.name=FALSE)
+
+
 #
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 #
-# Reading the list of the 561 measurements
-features <- read.table("features.txt")
 
-# Identifying the indexes of the features that are in scope
-selected_indexes<-grep("*mean()|*std()",features$V2)
-# Getting the indexed and names of the features in scope
-selected_features<-features[selected_indexes,]
 
-# Extracting the measurements that are in scope
-selected_X<-X[,selected_indexes]
+# Identifying the columns names for the subject, the activity_id and the features that are in scope
+selected_indexes<-grep("subject|activity_id|*mean()|*std()",colnames(data))
+
+# Extracting the columns we want to keep
+selected_data<-data[,selected_indexes]
 
 
 #
 # 3. Uses descriptive activity names to name the activities in the data set
 #
 # Reading the list of activity labels
-activity_labels<-read.table("activity_labels.txt")
+activity_labels<-read.table("activity_labels.txt",col.names = c("activity_id", "activity"))
 
-# In y and as well in ctivity_labels, the label identifiers are in the column V1. The merge will therefore use V1 as the key
-y_labels<-merge(y,activity_labels)
+# In y and as well in ctivity_labels, the label identifiers are in the column activity_id
+# The merge will therefore use activity_id as the key
+merged_data<-merge(selected_data,activity_labels)
 
 
 #
 # 4. Appropriately labels the data set with descriptive variable names.
-# 
-# Renaming the selected_X variable names with the features names
-colnames(selected_X)<-selected_features[,2]
+# colnames(
 
-# The data frame "subject" has 10299 observations of 1 variable: the subject who performed the activity for each window sample.
-# The data frame "y_labels" has 10299 observations of 2 variable: 
-#                                      V1 is the id and V2 is the description of the activity performed for each window sample.
-# The data frame "selected_X" has 10299 observation of 79 mean and std variables recorded by the phone sensor
-#                                      tBodyAcc-mean()-Y..fBodyBodyGyroJerkMag-meanFreq()
-#
-# Binding the subject, activity and features together as one data frame
-data<-cbind(subject=subject$V1,activity=y_labels$V2,selected_X)
-
-test<-cbind(subject=subject$V1,activity=y_labels$V2)
-write.table(test,file="test.txt", row.name=FALSE)
-
+# not needed anymore, the columns names has already been named
 
 #
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 #
+# List the name of the measurements that are in scope
+selected_features<-grep("*mean()|*std()",colnames(merged_data))
+
 # To facilicate averaging, we want to have only one measure per record. So we melt our data frame by subject and activity:
-#dataMelt<-melt(data,id=c("subject","activity"),measure.vars=selected_features[,2])
+dataMelt<-melt(merged_data,id=c("subject","activity"),measure.vars=selected_features)
 
 # Calculating the average by subject and activity for each variable
 averages<-dcast(dataMelt,subject + activity ~ variable, mean)
@@ -105,7 +112,8 @@ averages<-dcast(dataMelt,subject + activity ~ variable, mean)
 write.table(averages,file="averages.txt", row.name=FALSE)
 
 # Automatically generates a code book about the averages data file
-makeCodebook(averages)
+# Needed only once for documentation purpose
+# makeCodebook(averages)
 
 
 
